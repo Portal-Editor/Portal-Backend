06d3cda7aa048dbc562f5f572557e6b0bc7288a2
kevinzwithu <zzkevin@126.com>
2018-05-13 18:18:15 +0800

    Authorization mission start

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(3000);
+
 /* Dealing with authorization processes. */
 router.get('/', function(req, res, next) {
   res.send('Test: get authorization status');

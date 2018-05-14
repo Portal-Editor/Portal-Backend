5aeeefb7b89bb711ef9622d1afdced4095e1ab11
kevinzwithu <zzkevin@126.com>
2018-05-14 23:22:24 +0800

    Bug fixed

--- routes/share.js
+++ routes/share.js
@@ -286,32 +286,36 @@ function judgeType(ws, msg, stream) {
         //     if (err) throw err;
         //     console.log('The "data to append" was appended to file!');
         // });
-        console.log("File length: " + data.data.length);
-        yauzl.fromBuffer(data.data, {
-            lazyEntries: false,
-            decodeStrings: true,
-            validateEntrySizes: true
-        }, (err, zipfile) => {
-            if (err) throw err;
-            zipfile.readEntry();
-            zipfile.on("entry", function (entry) {
-                if (/\/$/.test(entry.fileName)) {
-                    // Directory file names end with '/'.
-                    // Note that entires for directories themselves are optional.
-                    // An entry's fileName implicitly requires its parent directories to exist.
-                    zipfile.readEntry();
-                } else {
-                    // file entry
-                    zipfile.openReadStream(entry, function (err, readStream) {
-                        if (err) throw err;
-                        readStream.on("end", function () {
-                            zipfile.readEntry();
+        try {
+            console.log("File length: " + data.data.length);
+            yauzl.fromBuffer(data.data, {
+                lazyEntries: false,
+                decodeStrings: true,
+                validateEntrySizes: true
+            }, (err, zipfile) => {
+                if (err) throw err;
+                zipfile.readEntry();
+                zipfile.on("entry", function (entry) {
+                    if (/\/$/.test(entry.fileName)) {
+                        // Directory file names end with '/'.
+                        // Note that entires for directories themselves are optional.
+                        // An entry's fileName implicitly requires its parent directories to exist.
+                        zipfile.readEntry();
+                    } else {
+                        // file entry
+                        zipfile.openReadStream(entry, function (err, readStream) {
+                            if (err) throw err;
+                            readStream.on("end", function () {
+                                zipfile.readEntry();
+                            });
+                            readStream.pipe("/root/kevinz/portals/");
                         });
-                        readStream.pipe("/root/kevinz/portals/");
-                    });
-                }
+                    }
+                });
             });
-        });
+        } catch (err) {
+            console.log("Errors occur:" + err);
+        }
     } else {
         // OT
         console.log(JSON.stringify(data));
